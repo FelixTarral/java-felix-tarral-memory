e869d870802810824de82851d81038014a2137f6
Felix <tarralfelix@gmail.com>
2019-05-24 12:19:45 +0200

    creation

---
+++ README.md
@@ -0,0 +1,1 @@
+# java-felix-tarral-memory
---
+++ src/com/example/carte/EtatMemoire.java
@@ -0,0 +1,43 @@
+package com.example.carte;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JToggleButton;
+
+public class EtatMemoire {
+
+	private List<JToggleButton> listeBoutonsSelectionnes = new ArrayList<>();
+
+	private void verifierNombreCartesRetournees() {
+		if (listeBoutonsSelectionnes.size() >= 2) {
+			for (JToggleButton jToggleButton : listeBoutonsSelectionnes) {
+				jToggleButton.setSelected(false);
+			}
+			// vide la liste
+			listeBoutonsSelectionnes.clear();
+		}
+	}
+
+	private void verifierCartesIdentiques() {
+		if (listeBoutonsSelectionnes.size() == 2) {
+			if (listeBoutonsSelectionnes.get(0).getClientProperty("carte")
+					.equals(listeBoutonsSelectionnes.get(1).getClientProperty("carte"))) {
+				for (JToggleButton jToggleButton : listeBoutonsSelectionnes) {
+					jToggleButton.setEnabled(false);
+				}
+				listeBoutonsSelectionnes.clear();
+			}
+		}
+	}
+
+	public void nouveauBoutonSelectionne(JToggleButton button) {
+		if (!listeBoutonsSelectionnes.contains(button)) {
+			verifierNombreCartesRetournees();
+
+			listeBoutonsSelectionnes.add(button);
+
+			verifierCartesIdentiques();
+		}
+	}
+}
